82d181561668f299d5ede28950b5699610d7770a
Ернар Пупкин <gamerhey2@gmail.com>
2024-09-15 21:05:58 +0500

    Task2

---
+++ Tyuiu.FilimonchukED.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.FilimonchukED.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.FilimonchukED.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Егор"));
+            Console.ReadKey();
+        }
+    }
+}
